5486c1cda92207fdc874005a609748153ddbc607
Leonard Schröter <leonardschroeter@gmail.com>
2021-09-04 16:27:57 +0200

    Add example where the gradient does not get taken unto account when computing the gradient of the accumulated loss

--- QuadraticFunction.py
+++ QuadraticFunction.py
@@ -11,3 +11,16 @@ class QuadraticFunction(keras.layers.Layer):
 
     def call(self, inputs):
         return tf.norm(tf.linalg.matvec(self.W, inputs) - self.y)
+
+class QuadraticFunctionLayer(keras.layers.Layer):
+    def __init__(self, dimension, **kwargs):
+        super(QuadraticFunctionLayer, self).__init__(**kwargs)
+        self.dimension = dimension
+        self.W = tf.random.normal([dimension, dimension])
+        self.y = tf.random.normal([dimension])
+
+    def build(self, input_shape):
+        self.theta = self.add_weight(name="theta", shape=[self.dimension], dtype=tf.float32, initializer="random_normal", trainable=True)
+    
+    def call(self, inputs=tf.zeros([1])):
+        return tf.norm(tf.linalg.matvec(self.W, self.theta) - self.y)
--- main.py
+++ main.py
@@ -8,9 +8,10 @@ import tensorflow as tf
 from tensorflow import keras
 
 from LSTMNetworkPerParameter import LSTMNetworkPerParameter
-from QuadraticFunction import QuadraticFunction
+from QuadraticFunction import QuadraticFunction, QuadraticFunctionLayer
 
 
+# accumulate losses and train optimizer once for a single objective function
 def example_1():
     optimizer_network = LSTMNetworkPerParameter()
     optimizer_optimizer = keras.optimizers.Adam()
@@ -46,6 +47,7 @@ def example_1():
 
         optimizer_network.reset_states()
 
+# accumulate the last T losses and train optimizer in each step of the optimization of the objective function
 def example_2(T):
     optimizer_network = LSTMNetworkPerParameter()
     optimizer_optimizer = keras.optimizers.Adam()
@@ -58,13 +60,14 @@ def example_2(T):
         theta = tf.random.normal([10])
         
         optimizer_network.reset_states()
+        losses.clear()
 
         with tf.GradientTape(persistent=True) as tape:
             with tf.GradientTape(persistent=True) as tape2:
                 tape2.watch(theta)
 
                 first = True
-                for step in range(40):
+                for step in range(200):
 
                     loss = quadratic_function(theta)
                     losses.append(loss)
@@ -87,5 +90,42 @@ def example_2(T):
                             optimizer_gradients = tape.gradient(optimizer_loss, optimizer_network.trainable_weights)
                             optimizer_optimizer.apply_gradients(zip(optimizer_gradients, optimizer_network.trainable_weights))
 
+# dont take the input to the optimizer network into account when computing the gradient of the loss
+def example_3():
+    optimizer_network = LSTMNetworkPerParameter()
+    optimizer_optimizer = keras.optimizers.Adam()
+
+    for training_step in range(100_000):
+        print("TRAINING STEP: ", training_step)
+        quadratic_function = QuadraticFunction(10)
+        theta = tf.random.normal([10])
+
+        with tf.GradientTape(persistent=True) as tape:
+            tape.watch(theta)
+
+            optimizer_loss = tf.zeros([1], tf.float32)
+
+            for step in range(50):
+                loss = quadratic_function(theta)
+
+                with tape.stop_recording():
+                    gradients = tape.gradient(loss, theta)
+
+                gradients = tf.stop_gradient(gradients)
+
+                optimizer_output = optimizer_network(gradients)
+
+                theta = theta + optimizer_output
+
+                optimizer_loss = optimizer_loss + loss
+
+                if step % 10 == 0:
+                    print("  Loss: ", loss.numpy())
+
+        optimizer_gradients = tape.gradient(optimizer_loss, optimizer_network.trainable_weights)
+        optimizer_optimizer.apply_gradients(zip(optimizer_gradients, optimizer_network.trainable_weights))
+
+        optimizer_network.reset_states()
+
 if __name__ == "__main__":
-    example_2(20)
+    example_3()
