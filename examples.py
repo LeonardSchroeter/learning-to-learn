1a894e25ab75e512cf7dd629a57f03bd15a6ee83
Leonard Schröter <leonardschroeter@gmail.com>
2021-09-21 00:10:18 +0200

    Update to work with neural network objective

--- examples.py
+++ examples.py
@@ -5,13 +5,11 @@ from tensorflow import keras
 class MLP(keras.layers.Layer):
     def __init__(self):
         super(MLP, self).__init__()
-        self.linear_1 = keras.layers.Linear(32)
-        self.linear_2 = keras.layers.Linear(32)
-        self.linear_3 = keras.layers.Linear(10)
+        self.linear_1 = keras.layers.Dense(32, activation="relu")
+        self.linear_2 = keras.layers.Dense(32, activation="relu")
+        self.linear_3 = keras.layers.Dense(10, activation="softmax")
 
     def call(self, inputs):
         x = self.linear_1(inputs)
-        x = tf.nn.relu(x)
         x = self.linear_2(x)
-        x = tf.nn.relu(x)
         return self.linear_3(x)
--- train.py
+++ train.py
@@ -14,11 +14,12 @@ from QuadraticFunction import QuadraticFunctionLayer
 
 
 class LearningToLearn():
-    def __init__(self, optimizer_network, objective_network_generator, objective_loss_fn, accumulate_losses = tf.add_n):
+    def __init__(self, optimizer_network, objective_network_generator, objective_loss_fn, dataset, accumulate_losses = tf.add_n):
         self.optimizer_network = optimizer_network
         self.objective_network_generator = objective_network_generator
         self.objective_loss_fn = objective_loss_fn
         self.objective_network_weights = {}
+        self.dataset = dataset
         self.accumulate_losses = accumulate_losses
 
     def clear_weights(self):
@@ -27,7 +28,7 @@ class LearningToLearn():
     def apply_weight_changes(self, g_t):
         for layer, g_t_layer in zip(self.objective_network_weights.keys(), g_t):
             for name, g_t_tensor in zip(self.objective_network_weights[layer].keys(), g_t_layer):
-                self.objective_network_weights[layer][name] = self.objective_network_weights[layer][name] + g_t_tensor
+                self.objective_network_weights[layer][name] = self.objective_network_weights[layer][name] + 0.01 * g_t_tensor
 
     def custom_getter(self, layer_name):
         def _custom_getter(name, **kwargs):
@@ -54,42 +55,44 @@ class LearningToLearn():
     def custom_call(self):
         original_call = keras.layers.Layer.__call__
         def _custom_call(other, *args, **kwargs):
-            for name, weight in self.objective_network_weights[other.name].items():
-                if hasattr(other, name):
-                    setattr(other, name, weight)
+            if other.name in self.objective_network_weights:
+                for name, weight in self.objective_network_weights[other.name].items():
+                    if hasattr(other, name):
+                        setattr(other, name, weight)
             return original_call(other, *args, **kwargs)
         return _custom_call
 
-    def train_optimizer(self, steps = 1_000_000):
+    def train_optimizer(self):
         optimizer_optimizer = keras.optimizers.Adam()
 
-        for step in range(steps):
+        for step, batch in self.dataset.enumerate():
+            print("Step: ", step.numpy())
             self.optimizer_network.reset_states()
             self.clear_weights()
             with mock.patch.object(keras.layers.Layer, "add_weight", self.custom_add_weight()):
                 objective_network = self.objective_network_generator()
-            optimizer_gradients = self.train_objective(objective_network, 50)
-            optimizer_optimizer.apply_gradients(zip(optimizer_gradients, self.optimizer_network.trainable_weights))
+            self.train_objective(objective_network, optimizer_optimizer, batch)
 
-    def train_objective(self, objective_network, T = 20):
+    def train_objective(self, objective_network, optimizer_optimizer, batch, T = 16):
         losses = deque(maxlen=T)
 
         with tf.GradientTape(persistent=True) as tape:
-            for step in range(T):
+            for step, (x, y) in tf.data.Dataset.from_tensor_slices(batch).enumerate().as_numpy_iterator():
+                if not objective_network.built:
+                    with mock.patch.object(keras.layers.Layer, "add_weight", self.custom_add_weight()):
+                        building_output = objective_network(x)
+
                 tape.watch(self.objective_network_weights)
 
-                with mock.patch.object(keras.layers.Layer, "add_weight", self.custom_add_weight()):
-                    with mock.patch.object(keras.layers.Layer, "__call__", self.custom_call()):
-                        outputs = objective_network(tf.zeros([1]))
-                
-                loss = self.objective_loss_fn(outputs, None)
+                with mock.patch.object(keras.layers.Layer, "__call__", self.custom_call()):
+                    outputs = objective_network(x)
+
+                loss = self.objective_loss_fn(y, outputs)
 
                 losses.append(loss)
 
                 with tape.stop_recording():
                     gradients = tape.gradient(loss, self.objective_network_weights)
-
-                gradients = [list(grads.values()) for grads in gradients.values()]
 
                 # TODO Find out whether this line is needed.
                 # I think it doesn't, since computing the gradients inside stop_recording
@@ -99,23 +102,36 @@ class LearningToLearn():
                 # gradients = tf.stop_gradient(gradients)
 
                 optimizer_output = self.optimizer_network(gradients)
-
                 self.apply_weight_changes(optimizer_output)
-            
-            optimizer_loss = self.accumulate_losses(losses)
-            print(loss.numpy())
-        
-        optimizer_gradients = tape.gradient(optimizer_loss, self.optimizer_network.trainable_weights)
+                
+                if (step + 1) % T == 0:
+                    print("  Loss: ", loss.numpy())
+                    optimizer_loss = self.accumulate_losses(losses)
+                    with tape.stop_recording():
+                        optimizer_gradients = tape.gradient(optimizer_loss, self.optimizer_network.trainable_weights)
+                        optimizer_optimizer.apply_gradients(zip(optimizer_gradients, self.optimizer_network.trainable_weights))
+                    losses.clear()
+                    tape.reset()
 
-        return list(optimizer_gradients)
 
 def main():
     tf.random.set_seed(1)
-    objective_network_generator = lambda : QuadraticFunctionLayer(10)
-    # objective_network_generator = lambda : MLP()
-    objective_loss_fn = lambda x, y: x
+
+    (x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
+    dataset = tf.data.Dataset.from_tensor_slices(
+        (x_train.reshape(60000, 784).astype("float32") / 255, y_train)
+    )
+    # dataset = tf.data.Dataset.from_tensor_slices(
+    #     (tf.zeros([60000, 784]), tf.zeros([60000]))
+    # )
+    dataset = dataset.shuffle(buffer_size=1024).batch(1).batch(2048)
+
+    # objective_network_generator = lambda : QuadraticFunctionLayer(10)
+    objective_network_generator = lambda : MLP()
+    # objective_loss_fn = lambda x, y: y
+    objective_loss_fn = keras.losses.SparseCategoricalCrossentropy()
     optimizer_network = LSTMNetworkPerParameter()
-    ltl = LearningToLearn(optimizer_network, objective_network_generator, objective_loss_fn)
+    ltl = LearningToLearn(optimizer_network, objective_network_generator, objective_loss_fn, dataset)
     ltl.train_optimizer()
 
 if __name__ == "__main__":
