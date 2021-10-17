52b1fd7d5779234f06de727f11b05908202f5adc
LeonardSchroeter <leonardschroeter@gmail.com>
2021-10-17 22:28:20 +0200

    Add learning rate and remove bias of dense layer

--- optimizer_network.py
+++ optimizer_network.py
@@ -3,11 +3,13 @@ from tensorflow import keras
 
 
 class LSTMNetworkPerParameter(keras.Model):
-    def __init__(self, **kwargs):
+    def __init__(self, learning_rate, **kwargs):
         super(LSTMNetworkPerParameter, self).__init__(**kwargs)
         self.lstm1 = keras.layers.LSTM(20, stateful=True, return_sequences=True)
         self.lstm2 = keras.layers.LSTM(20, stateful=True)
-        self.output_layer = keras.layers.Dense(1)
+        self.output_layer = keras.layers.Dense(1, use_bias=False)
+
+        self.learning_rate = learning_rate
 
     def reset_states(self):
         if self.lstm1.built and self.lstm2.built:
@@ -20,5 +22,5 @@ class LSTMNetworkPerParameter(keras.Model):
         x = tf.reshape(inputs, [size, 1, 1])
         x = self.lstm1(x)
         x = self.lstm2(x)
-        x = 0.01 * self.output_layer(x)
+        x = self.learning_rate * self.output_layer(x)
         return tf.reshape(x, [size])
